59839f6b874f515fbf61feec886da1e8d208596c
thushanK <ktwimalaweera@gmail.com>
2021-08-01 08:21:43 +0530

    add custome css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
-
+import React from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import Router from './Routes/Router'
+import './custom.scss'
+import './custom.css'
+// import { toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
